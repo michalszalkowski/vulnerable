309ce311182c642da98a95c1feea354c1abbfbaa
MichalSzalkowski <msbtbw@gmail.com>
2023-10-02 14:07:34 +0200

    vun app

--- src/main/java/com/michalszalkowski/vulnerable/module/SqlController.java
+++ src/main/java/com/michalszalkowski/vulnerable/module/SqlController.java
@@ -12,7 +12,6 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.websocket.server.PathParam;
 import java.io.IOException;
 import java.util.List;
 
